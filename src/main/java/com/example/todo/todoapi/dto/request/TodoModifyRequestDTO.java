0b087b17df0f0711410c5196d4102e3ad63e59f9
minah8 <o_uo_o@naver.com>
2024-05-17 12:08:58 +0900

    #2. todo api 할 일 기능 작성 완료

---
+++ src/main/java/com/example/todo/todoapi/dto/request/TodoModifyRequestDTO.java
@@ -0,0 +1,19 @@
+package com.example.todo.todoapi.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoModifyRequestDTO {
+
+    @NotBlank
+    private String id;
+
+    private boolean done;
+}
