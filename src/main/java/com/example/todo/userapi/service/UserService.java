cd89574ba0b9da127927fd4ae5f624101feba52d
minah8 <o_uo_o@naver.com>
2024-05-20 15:55:37 +0900

    #4. 회원 가입 처리

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -1,10 +1,19 @@
 package com.example.todo.userapi.service;
 
+import com.example.todo.userapi.dto.request.LoginRequestDTO;
+import com.example.todo.userapi.dto.request.UserSignUpRequestDTO;
+import com.example.todo.userapi.dto.response.UserSignUpResponseDTO;
+import com.example.todo.userapi.entity.User;
 import com.example.todo.userapi.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -13,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean isDuplicate(String email) {
         if (userRepository.existsByEmail(email)) {
@@ -20,4 +30,24 @@ public class UserService {
             return true;
         } else return false;
     }
+
+    public UserSignUpResponseDTO create(final UserSignUpRequestDTO dto) throws Exception {
+        String email = dto.getEmail();
+
+        if (isDuplicate(email)) {
+            throw new RuntimeException("중복된 이메일 입니다.");
+        }
+
+        // 패스워드 인코딩
+        String encode = passwordEncoder.encode(dto.getPassword());
+        dto.setPassword(encode);
+
+        // dto를 User Entity로 변환해서 저장
+        User saved = userRepository.save(dto.toEntity());
+        log.info("회원 가입 정상 수행됨! - saved uesr - {}", saved);
+
+        return new UserSignUpResponseDTO(saved);
+
+    }
+
 }
